c275840aedc0bc460b976b3fa4de1f804fceeffd
yukieiji <yukieiji@users.noreply.github.com>
2022-09-19 20:00:52 +0900

    fix:visor_update

--- update_visor_data.py
+++ update_visor_data.py
@@ -8,12 +8,13 @@ WORKING_DIR = os.path.dirname(os.path.realpath(__file__))
 VISOR_IN_FILE = os.path.join(WORKING_DIR, 'VisorTransData.xlsx')
 VISOR_FOLDER = os.path.join(WORKING_DIR, 'visor')
 VISOR_OUT_FILE = os.path.join(WORKING_DIR, 'visor', 'visorTransData.json')
-VISOR_DATA_FILE = os.path.join(WORKING_DIR, 'visor', '')
+VISOR_DATA_FILE = os.path.join(WORKING_DIR, 'visor', 'visorData.json')
 
+VISOR_LICENSE = 'LICENSE.md'
 VISOR_DATA_DATA_KEY = 'data'
 VISOR_DATA_UPDATE_COMIT_KEY = 'updateComitHash'
 
-def stringToJson(filename, outputFile):
+def stringToJson(filename : str, outputFile : str) -> None:
 
   wb = load_workbook(filename, read_only = True)
 
@@ -44,7 +45,7 @@ def stringToJson(filename, outputFile):
   with open(outputFile, "w") as f:
     json.dump(stringData, f, indent=4)
 
-def update_visor_data():
+def update_visor_data()-> None:
 
   visor_dir = os.listdir(VISOR_FOLDER)
 
@@ -56,15 +57,17 @@ def update_visor_data():
     if os.path.isdir(os.path.join(VISOR_FOLDER, visor)):
 
       visors = os.listdir(os.path.join(VISOR_FOLDER, visor))
-      new_visor[visor] = visors
+      cleaned_visor = [add_visor.replace('.png', '') for add_visor in visors if add_visor != VISOR_LICENSE]
+      new_visor[visor] = cleaned_visor
 
 
-  with open(os.path.join(VISOR_DATA_FILE), mode='w') as hat_json:
+  with open(os.path.join(VISOR_DATA_FILE), mode='w') as visor_json:
     json.dump(
       new_visor,
-      hat_json, indent=2, ensure_ascii=False)
+      visor_json, indent=2, ensure_ascii=False)
 
 def main()-> None:
+  update_visor_data()
   stringToJson(VISOR_IN_FILE, VISOR_OUT_FILE)
 
 if __name__ == "__main__":
